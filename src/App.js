a6ee0984cb32331f2a409d2326d954253fd81614
melissaabiaad <melissaabiaadofficial@gmail.com>
2026-05-01 17:15:16 +0300

    Adding admin frontend pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from "./pages/authentication/Login";
 
-import InvestorDashboard from "./pages/InvestorDashboard";
-import AdminDashboard from "./pages/AdminDashboard";
-import CompanyDashboard from "./pages/CompanyDashboard";
+import InvestorDashboard from "./pages/investor/InvestorDashboard";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminPendingDeals from "./pages/admin/AdminPendingDeals";
+import CompanyDashboard from "./pages/company/CompanyDashboard";
+import AdminPendingCompanies from "./pages/admin/AdminPendingCompanies";
+import AdminKYCReview from "./pages/admin/AdminKYCReview";
+import AdminAuditLogs from "./pages/admin/AdminAuditLogs";
+import Portfolio from "./pages/investor/Portfolio";
 
 function App() {
   return (
@@ -12,6 +17,11 @@ function App() {
       <Route path="/investor-dashboard" element={<InvestorDashboard />} />
       <Route path="/admin-dashboard" element={<AdminDashboard />} />
       <Route path="/company-dashboard" element={<CompanyDashboard />} />
+      <Route path="/pending-deals" element={<AdminPendingDeals />} />
+      <Route path="/pending-companies" element={<AdminPendingCompanies/>}/>
+      <Route path="/kyc-review" element={<AdminKYCReview/>}/>
+      <Route path="/audit-logs" element={<AdminAuditLogs/>}/>
+      <Route path="/portfolio" element={<Portfolio/>}/>
     </Routes>
   );
 }
